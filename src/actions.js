ac242d2db6528b15f81e52b0f9e508d040ab1d85
Zuzia Zuzewicz <zuzannazuzewicz@MacBook-Air-Zuzia.local>
2018-02-20 00:06:31 +0100

    Added project

---
+++ src/actions.js
@@ -0,0 +1,32 @@
+const ADD_COMMENT = 'ADD_COMMENT';
+const THUMB_UP_COMMENT = 'THUMB_UP_COMMENT';
+const THUMB_DOWN_COMMENT = 'THUMB_DOWN_COMMENT';
+
+function addComment(text) {
+    return {
+        type: ADD_COMMENT,
+        id: uuid.v4(),
+        text: text
+    }
+}
+
+function thumbUpComment(commentId) {
+    return {
+        type: THUMB_UP_COMMENT,
+        id: commentId
+    }
+}
+
+function thumbDownComment(commentId) {
+    return {
+        type: THUMB_DOWN_COMMENT,
+        id: commentId
+    }
+}
+
+export ADD_COMMENT;
+export THUMB_UP_COMMENT;
+export THUMB_DOWN_COMMENT;
+export addComment;
+export thumbUpComment;
+export thumbDownComment;
---
+++ src/reducer.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import comments from './comments.js';
+import users from './users.js';
+
+const app = combineReducers({
+    comments,
+    users
+});
